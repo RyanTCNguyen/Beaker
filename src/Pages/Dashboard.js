b2f7d821c8e2e9e4dde403b77e36121bc160290f
Brittany S <57652157+bsteenbergen@users.noreply.github.com>
2022-09-23 11:30:34 -0700

    deleted unused imports from Pages

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -7,8 +7,6 @@ import { useAuth } from '../Contexts/authContext'
 import { db } from '../firebase'
 import { collection, getDocs } from 'firebase/firestore'
 
-// SM: If you are not using this commented out code,
-//     it would probably be best to get rid of it to keep the file cleaner
 function Dashboard({ sidebaritems, members }) {
     const [isStudent, setIsStudent] = useState(true)
     const { signin, currentUser } = useAuth()
@@ -37,24 +35,23 @@ function Dashboard({ sidebaritems, members }) {
     return (
         <Layout>
             <Side sidebaritems={sidebaritems}>
-            {users && (
-                <div>
-                    <h1
-                        id="dash-title"
-                        style={{
-                            color: 'rgba(16, 127, 183, 1)',
-                            textAlign: 'center',
-                            fontSize: '50px',
-                            fontWeight: 'lighter',
-                            paddingTop: '20rem',
-                        }}
-                    >
-                        {' '}
-                        <b> {name} </b>, welcome
-                        to your dashboard{' '}
-                    </h1>
-                </div>
-            )}
+                {users && (
+                    <div>
+                        <h1
+                            id="dash-title"
+                            style={{
+                                color: 'rgba(16, 127, 183, 1)',
+                                textAlign: 'center',
+                                fontSize: '50px',
+                                fontWeight: 'lighter',
+                                paddingTop: '20rem',
+                            }}
+                        >
+                            {' '}
+                            <b> {name} </b>, welcome to your dashboard{' '}
+                        </h1>
+                    </div>
+                )}
             </Side>
         </Layout>
     )
