381b4b571c4b93f151ea94cfd0a9c702cc84dc40
RyanTCNguyen <ryan13ng@gmail.com>
2022-11-02 21:10:42 -0700

    env var

--- src/EngineFunctions/ProjectsFetch.js
+++ src/EngineFunctions/ProjectsFetch.js
@@ -1,15 +1,26 @@
 export const getProjects = () => {
-    const AUTHKEY = process.env.ELASTIC_PRIVATE_API_KEY
-    const params =  {
+    const AUTHKEY = process.env.REACT_APP_ELASTIC_API_KEY
+    const USER = process.env.REACT_APP_ELASTIC_USERNAME
+    const PASSWORD = process.env.REACT_APP_ELASTIC_PASSWORD
+    const params = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Basic '+btoa('elastic:XyUFHX0uvoRKuBWnqYnwsGWD'),
+            Authorization: 'Basic ' + btoa('elastic:XyUFHX0uvoRKuBWnqYnwsGWD'),
         },
         mode: 'cors',
         body: JSON.stringify({
-            data: 1
+            data: 1,
         }),
     }
-    return fetch(`https://beaker.ent.us-central1.gcp.cloud.es.io/api/as/v1/engines/posts-engine/documents`,params).then((data)=>{return data.json()}).then((data)=>{console.log(data)})
+    return fetch(
+        `https://beaker.ent.us-central1.gcp.cloud.es.io/api/as/v1/engines/posts-engine/documents`,
+        params
+    )
+        .then((data) => {
+            return data.json()
+        })
+        .then((data) => {
+            console.log(data)
+        })
 }
