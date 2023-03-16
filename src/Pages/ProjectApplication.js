67a21fc1e55d09759c916aa1ee2d1ee5e8ba66b7
claytoncb <cbruce4@lion.lmu.edu>
2023-03-16 16:53:13 -0700

    Project Application Popup changes

--- src/Pages/ProjectApplication.js
+++ src/Pages/ProjectApplication.js
@@ -1,14 +1,23 @@
+import { TextareaAutosize, TextField } from "@material-ui/core"
 import { useState } from "react"
 import { postFunction } from "../EngineFunctions/ProjectsFetch"
 
-export default function ProjectApplication({id, username}) {
-    const defaultApplication = {reason: "reason here", id: id, name: username}
+export default function ProjectApplication({id, email}) {
+    const defaultApplication = {reason: "", projcet_id: id, email: email, accepted: false}
     const [application, setApplication] = useState(defaultApplication) 
-    const onSubmit=()=>{postFunction('apps-engine', application)}
+    const onSubmit=()=>{postFunction('applications-engine', application)}
     return(
-        <div style={{position:"absolute"}}>
-            <textarea value={application.reason} onChange={e=>setApplication({...application, reason: e.target.value})}/>
-            <button onClick={onSubmit}>post</button>
+        <div style={{
+            position:"absolute",
+            display: "inlineBlock",
+            cursor: "pointer",
+            top: "25vh",
+            left: "10vw",
+        }}>
+            <div style={{backgroundColor:"slategrey", zIndex: 11, width: "80vw", height: "50vh", padding: "20px"}}>
+                <TextField  style={{height:"6em !important"}}multiline={true} size='medium' placeholder="Reason you are a good fit" value={application.reason} onChange={e=>setApplication({...application, reason: e.target.value})}/>
+                <button onClick={onSubmit}>post</button>
+            </div>
         </div>
     )
 }
