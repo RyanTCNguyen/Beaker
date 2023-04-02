c4d2a6c4d4ded86805f20a8e1c9888d46c371951
claytoncb <cbruce4@lion.lmu.edu>
2023-04-02 00:55:22 -0700

    fixed distortion

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -6,7 +6,7 @@ const Layout = ({ children, blur = true }) => {
     return (
         <>
             <Navbar />
-            <img style={{width: '102vw', height: '100%', position:'fixed',top:'3.5rem',left:'-1rem', zIndex: -10, filter: blur?'blur(6px)':'blur(0px)'}} src={campusBackground} alt="LMU Campus"/>
+            <img style={{width: '102vw', height: 'auto', position:'fixed',top:'-2rem',left:'-1rem', zIndex: -10, filter: blur?'blur(6px)':'blur(0px)'}} src={campusBackground} alt="LMU Campus"/>
 
             <div style={{paddingTop:"10vh"}}>{children}</div>
         </>
