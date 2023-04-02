3c3d16c3303cc35ee1f04666e68cf8c39f6c3d75
claytoncb <cbruce4@lion.lmu.edu>
2023-04-01 19:59:00 -0700

    possible background?

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Navbar from './Navbar'
 import lMUCampus from '../Images/LMUCampus.jpg'
 
-const Layout = ({ children }) => {
+const Layout = ({ children, blur = true }) => {
     return (
         <>
             <Navbar />
-            <img style={{width: '99vw', height: '99vh', position:'fixed', zIndex: -10}} src={lMUCampus} alt="LMU Campus"/>
+            <img style={{width: '99vw', height: '99vh', position:'fixed', zIndex: -10, filter: blur?'blur(6px)':'blur(0px)'}} src={lMUCampus} alt="LMU Campus"/>
 
             <div style={{paddingTop:"10vh"}}>{children}</div>
         </>
